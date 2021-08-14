5518c5939d5c287b6215e108e78e8d0b30dcdf1c
chaminwoooo <72906702+chaminwoooo@users.noreply.github.com>
2021-08-14 18:52:07 +0900

    bg-image 추가

--- nomard_momentum_clone/js/momentum.js
+++ nomard_momentum_clone/js/momentum.js
@@ -13,3 +13,13 @@ function GetClock() {
 // 함수 선언 : 선언하지 않으면 동작하지 않는다.
 GetClock();
 setInterval(GetClock, 1000);
+
+//background-image
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"]
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `../images/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+bgImage.classList.add("bg-img");
