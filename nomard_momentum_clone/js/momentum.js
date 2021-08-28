cf895628334e25bbb6fc928b750830d70fc93cd2
chaminwoooo <72906702+chaminwoooo@users.noreply.github.com>
2021-08-28 11:47:10 +0900

    greeting

--- nomard_momentum_clone/js/momentum.js
+++ nomard_momentum_clone/js/momentum.js
@@ -23,3 +23,34 @@ bgImage.src = `../images/${chosenImage}`;
 
 document.body.appendChild(bgImage);
 bgImage.classList.add("bg-img");
+
+//Greeting 사용자 이름 넣기
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+// 주로 사용하는값 ? 변하지 않는 값은 대문자로 표현
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit',onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+}
